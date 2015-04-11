afd6dc97fce7519808e5b262974d4067370a5544
Alex <alex.tzinov@gmail.com>
2015-04-11 10:38:20 -0600

    trying to make it work

--- classyapp/app/controllers/assignments_controller.rb
+++ classyapp/app/controllers/assignments_controller.rb
@@ -1,9 +1,11 @@
 class AssignmentsController < ApplicationController
 
 	def index
+        @assignments = Assignment.all
 	end
 
 	def show
+        @assignment = Assignment.find(params[:id])
 	end
 
 	def new
--- classyapp/app/controllers/courses_controller.rb
+++ classyapp/app/controllers/courses_controller.rb
@@ -1,20 +1,31 @@
 class CoursesController < ApplicationController
 
 	def index
-		@course = Course.new
+		@assignemnts = Assignment.all
 	end
 
 	def show
+        @assignment = Assignment.find(params[:id])
 	end
 
 	def new
+        @assignment = Assignment.new
 	end
 
 	def edit
 	end
 
 	def create
+        @assignment = Assignment.new(assignment_params)
+
+        if @assignment.save
+            redirect_to @assignment
+        else
+            render 'new'
+        end
 	end
+
+    
 
 	def update
 	end
@@ -22,4 +33,9 @@ class CoursesController < ApplicationController
 	def destroy
 	end
 
+    private
+        def assignment_params
+            params.require(:assignment).permit(:name)
+        end
 end
+  
